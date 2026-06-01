532a15bf4d5a343ea8cc53681741fd9b615e2e93
JanGumbau <jan.gumbau@estudiant.enti.cat>
2026-06-01 09:26:51 +0200

    su

--- MyFirstOpenGL/MyFirstOpenGL/MyFirstOpenGL/MyFirstVertexShader.glsl
+++ MyFirstOpenGL/MyFirstOpenGL/MyFirstOpenGL/MyFirstVertexShader.glsl
@@ -5,30 +5,23 @@ layout(location = 0) in vec3 posicion;
 uniform mat4 translationMatrix;
 uniform mat4 rotationMatrix;
 uniform mat4 scaleMatrix;
-
-// Temps en segons des de l'inici del programa.
-// L'utilitzem per fer girar la noria.
 uniform float time;
 
 void main() {
 
-    // Velocitat de rotacio (radians per segon)
-    float velocitat = 0.8;
+    float velocitat = 0.8; // radians per segon
     float angle = time * velocitat;
+    float sinus = sin(angle);
+    float cosinus = cos(angle);
 
-    // Matriu de rotacio al voltant de l'eix Z (la noria gira al pla XY)
-    float c = cos(angle);
-    float s = sin(angle);
-
+ 
     mat4 rotacioNoria = mat4(
-        vec4( c,  s, 0.0, 0.0),
-        vec4(-s,  c, 0.0, 0.0),
-        vec4(0.0, 0.0, 1.0, 0.0),
-        vec4(0.0, 0.0, 0.0, 1.0)
+        vec4( cosinus, sinus, 0.0, 0.0),
+        vec4(-sinus, cosinus, 0.0, 0.0),
+        vec4( 0.0,    0.0,    1.0, 0.0),
+        vec4( 0.0,    0.0,    0.0, 1.0)
     );
 
-    // Apliquem la transformacio model: escalat -> rotacio noria -> translacio
     mat4 model = translationMatrix * rotacioNoria * scaleMatrix;
-
     gl_Position = model * vec4(posicion, 1.0);
 }
